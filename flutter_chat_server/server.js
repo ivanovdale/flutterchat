77dd9ae641429e9f580961198e19d249e9dd561a
dania-spb@mail.ru <dania-spb@mail.ru>
2022-05-31 19:10:49 +0300

    server.js - catching message

--- flutter_chat_server/server.js
+++ flutter_chat_server/server.js
@@ -3,7 +3,6 @@
 // noinspection JSUnusedLocalSymbols
 const users = {};
 
-
 // The collection of room descriptors of the current rooms on the server, keyed by name.  Each element is:
 //   roomName : { roomName : "", description : "", maxPeople : 99, private : true|false,
 //     creator : "",
@@ -15,6 +14,15 @@ const users = {};
 // noinspection JSUnusedLocalSymbols
 const rooms = {};
 
-
 // construct an HTTP server, wrapped in an Socket.IO server, and start it up.
 const io = require("socket.io")(require("http").createServer(function () { }).listen(80));
+
+// noinspection JSUnresolvedFunction
+/**
+ * Handle the socket connection event.  All other events must be hooked up inside this.
+ */
+io.on("connection", io => {
+
+
+    console.log("\n\nConnection established with a client");
+});
