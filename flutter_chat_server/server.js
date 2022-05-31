c38b31d0038ad40eae89759729b3c65917c14610
dania-spb@mail.ru <dania-spb@mail.ru>
2022-05-31 19:08:25 +0300

    server.js - begin

---
+++ flutter_chat_server/server.js
@@ -0,0 +1,20 @@
+// The collection of user descriptors currently known to the server, keyed by name.  Each element is:
+//   userName : { userName : "", password : "" }
+// noinspection JSUnusedLocalSymbols
+const users = {};
+
+
+// The collection of room descriptors of the current rooms on the server, keyed by name.  Each element is:
+//   roomName : { roomName : "", description : "", maxPeople : 99, private : true|false,
+//     creator : "",
+//     users : [
+//       userName : { userName : "" },
+//       ...
+//     ]
+//   }
+// noinspection JSUnusedLocalSymbols
+const rooms = {};
+
+
+// construct an HTTP server, wrapped in an Socket.IO server, and start it up.
+const io = require("socket.io")(require("http").createServer(function () { }).listen(80));
